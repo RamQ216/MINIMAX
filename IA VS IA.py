39fcd3c2f963c225dc4c1e3a32554fb1ccef5b04
RamQ216 <hugomramirez06@gmail.com>
2026-02-10 16:01:48 -0300

    Update IA VS IA.py

--- IA VS IA.py
+++ IA VS IA.py
@@ -31,7 +31,7 @@ def actualizar_laboratorio(fila, columna, ubi_gato, ubi_raton):
 def movimientos(fila, columna, pos_actual):
     f, c = pos_actual
     posibilidades = [(f - 1, c), (f + 1, c), (f, c - 1), (f, c + 1)]
-    validos = [mov for mov in posibilidades if 0 <= mov[0] < fila and 0 <= mov[1] < columna]
+    validos = [mov for mov in posibilidades if 0 <= mov[0] < fila and 0 <= mov[1] < columna]#1
     return validos
 
 
@@ -39,26 +39,26 @@ def movimientos(fila, columna, pos_actual):
 def minimax(pos_gato, pos_raton, profundidad, es_maximizando, f, c):
     # Caso base: El gato atrapa al ratón
     if pos_gato == pos_raton:
-        return -100 if es_maximizando else 100
+        return -100 if es_maximizando else 100#2
 
     # Caso base: Límite de "visión" del futuro
     if profundidad == 0:
         # Puntuación basada en Distancia Manhattan
-        return abs(pos_gato[0] - pos_raton[0]) + abs(pos_gato[1] - pos_raton[1])
+        return abs(pos_gato[0] - pos_raton[0]) + abs(pos_gato[1] - pos_raton[1])#3
 
     if es_maximizando:
         # Turno del RATÓN: Quiere ALEJARSE (Maximizar distancia)
-        mejor_valor = -math.inf
+        mejor_valor = -math.inf#4
         for mov in movimientos(f, c, pos_raton):
-            valor = minimax(pos_gato, mov, profundidad - 1, False, f, c)
-            mejor_valor = max(mejor_valor, valor)
+            valor = minimax(pos_gato, mov, profundidad - 1, False, f, c)#5
+            mejor_valor = max(mejor_valor, valor)#4
         return mejor_valor
     else:
         # Turno del GATO: Quiere ACERCARSE (Minimizar distancia)
         mejor_valor = math.inf
         for mov in movimientos(f, c, pos_gato):
-            valor = minimax(mov, pos_raton, profundidad - 1, True, f, c)
-            mejor_valor = min(mejor_valor, valor)
+            valor = minimax(mov, pos_raton, profundidad - 1, True, f, c)#5.1
+            mejor_valor = min(mejor_valor, valor)#4.1
         return mejor_valor
 
 
@@ -106,7 +106,7 @@ raton_pos = (random.randint(0, f - 1), random.randint(0, c - 1))
 while gato_pos == raton_pos:
     raton_pos = (random.randint(0, f - 1), random.randint(0, c - 1))
 
-max_turnos = 20
+max_turnos = 30
 turno = 0
 
 # --- BUCLE DE JUEGO (IA vs IA) ---
@@ -123,7 +123,7 @@ while not juego_terminado(gato_pos, raton_pos, turno, max_turnos):
     raton_pos = mov_inteligente_raton(raton_pos, gato_pos, f, c)
 
     turno += 1
-    time.sleep(0.5)  # Para poder ver el movimiento
+    time.sleep(0.55)  # Para poder ver el movimiento
 
 # Resultado final
 tablero_final = actualizar_laboratorio(f, c, gato_pos, raton_pos)
